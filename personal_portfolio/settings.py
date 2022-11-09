a241553abc1e1f9fee5f8121916ee0c04d65d621
cookies4u <jcreyes.1018@gmail.com>
2022-11-08 21:50:57 -0800

    test 2 fix server 500 error

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -200,3 +200,20 @@ if "CI" in os.environ:
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #
 # STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
