37e168f10100f8554dfd34b7eb31e6d1c2f8fe2f
cookies4u <jcreyes.1018@gmail.com>
2022-11-08 21:36:15 -0800

    adding static to aws

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -153,16 +153,21 @@ AWS_ACCESS_KEY_ID = 'AKIAU57MRGS6RL6JBY4B'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'portfoliojcr'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
