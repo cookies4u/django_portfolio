04fed998665d1d35b9ba8d6590a2899f5483a133
cookies4u <jcreyes.1018@gmail.com>
2022-11-08 20:42:59 -0800

    testing aws static file hosting

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -146,11 +146,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+AWS_ACCESS_KEY_ID = 'AKIAU57MRGS6RL6JBY4B'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'portfoliojcr'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 try:
     from .local_settings import *
