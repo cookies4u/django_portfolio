2ef8259fc9a98896cf3d5c0afa1111be07bf4f94
cookies4u <jcreyes.1018@gmail.com>
2022-11-13 16:03:06 -0800

    test

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -144,34 +144,42 @@ USE_TZ = True
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-AWS_ACCESS_KEY_ID = 'AKIAU57MRGS6RL6JBY4B'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'portfoliojcr'
+# aws static settings
+# AWS_ACCESS_KEY_ID = 'AKIAU57MRGS6RL6JBY4B'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'portfoliojcr'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+# AWS_DEFAULT_ACL = 'public-read'
 
-AWS_DEFAULT_ACL = 'public-read'
-
+# S3  static settings
 AWS_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# S3 public media settings
+# PUBLIC_MEDIA_LOCATION = 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+# DEFAULT_FILE_STORAGE = 'portfolio.storage_backends.PublicMediaStorage'
 
 # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
